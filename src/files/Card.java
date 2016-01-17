71f261ebf0320c52019f0af9847592b712a63501
samwest <samwest@umich.edu>
2016-01-16 19:18:28 -0500

    Updating Cards Class

--- src/files/Card.java
+++ src/files/Card.java
@@ -8,14 +8,65 @@ package files;
 public class Card {
 	private int cost;
 	private String desc;
+	private int turnsRemaining;
+	private int totalTurns;
+	private boolean isAction;
+	private boolean isVictory;
+	private boolean isTreasure;
+	private boolean isReaction;
+	
+	
 	
 	
 	/*** Create a new Card Object ***/
-	public Card(int cardCost,String descriptor){
+	public Card(int cardCost,String descriptor,int turns, boolean isAct,boolean isVic,boolean isTreas,boolean isReac){
 		cost = cardCost;
 		desc = descriptor;
+		totalTurns=turns;
+		turnsRemaining=turns;
+		isAction = isAct;
+		isVictory = isVic;
+		isTreasure = isTreas;
+		isReaction = isReac;
+		
 	}
 	
+	
+	// Called when the card is being added back to a deck or trashed
+	public void prepCardForDiscard() {
+		turnsRemaining=totalTurns;
+	}
+	
+	
+	// use a turn when the card is played
+	public void useTurn(){
+		turnsRemaining--;
+	}
+	
+	// the number of turns remaining for the card
+	public int numRemaining() {
+		return turnsRemaining;
+	}
+	
+	// if the card is an action card
+	public boolean isAction () {
+		return isAction;
+	}
+	
+	// if the card is a victory card
+	public boolean isVictory() {
+		return isVictory;
+	}
+	
+	// if the card is a treasure car
+	public boolean isTreasure() {
+		return isTreasure;
+	}
+	
+	// if the card is a reaction card
+	public boolean isReaction() {
+		return isReaction;
+	}
 	/*** Get the cost back ***/
 	public int getCost(){
 		return cost;
